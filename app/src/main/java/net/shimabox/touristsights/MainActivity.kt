a2f84a032d3f11d74b06804dc9e2ab02375228de
shimabox <shimabox@gmail.com>
2020-12-28 11:46:18 +0900

    :sparkles: MainActivityからListFragmentを呼び出す

--- app/src/main/java/net/shimabox/touristsights/MainActivity.kt
+++ app/src/main/java/net/shimabox/touristsights/MainActivity.kt
@@ -2,10 +2,23 @@ package net.shimabox.touristsights
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import net.shimabox.touristsights.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val tag = "ListFragment"
+        var fragment = supportFragmentManager.findFragmentByTag(tag)
+        if (fragment == null) {
+            fragment = ListFragment()
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.content, fragment, tag)
+            }.commit()
+        }
     }
 }
