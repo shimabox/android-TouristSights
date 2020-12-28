4614c676ef8801f4bfa090d7b1c0ec2f751bb970
shimabox <shimabox@gmail.com>
2020-12-28 10:17:33 +0900

    :sparkles: jsonデータの変換クラス

---
+++ app/src/main/java/net/shimabox/touristsights/Sight.kt
@@ -0,0 +1,26 @@
+package net.shimabox.touristsights
+
+import android.content.res.Resources
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.builtins.list
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonConfiguration
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+@Serializable
+class Sight(
+    val name: String,
+    val imageName: String,
+    val description: String,
+    val kind: String
+)
+
+fun getSights(resources: Resources): List<Sight> {
+    val assetManager = resources.assets
+    val inputStream = assetManager.open("sights.json")
+    val bufferedReader = BufferedReader(InputStreamReader(inputStream))
+    val str: String = bufferedReader.readText()
+    val obj = Json(JsonConfiguration.Stable).parse(Sight.serializer().list, str)
+    return obj
+}
